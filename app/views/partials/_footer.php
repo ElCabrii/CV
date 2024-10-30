2fae1e05ec30ab962f3bcf2ae5ac0729f72eae33
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-10-30 17:01:42 +0100

    update footer to include license button and improve accessibility

--- app/views/partials/_footer.php
+++ app/views/partials/_footer.php
@@ -1,5 +1,5 @@
-<footer>
-    <p>&copy; 2024 - Tous droits réservés</p>
+<footer id="footer">
+    <button id='license'>&copy; 2024 All rights reserved.</button>
     <button onclick="window.location.href='/index.php?page=termsandconditions'">Terms and Conditions</button>
     <button onclick="window.location.href='/index.php?page=privacy'">Privacy Policy</button>
 </footer>
