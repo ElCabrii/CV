b9a11f154cd24f7d82bd5c71e46d51b099510632
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-10-30 17:01:11 +0100

    refactor header navigation for improved accessibility and user experience

--- app/views/partials/_header.php
+++ app/views/partials/_header.php
@@ -1,17 +1,16 @@
 <header>
     <button onclick="window.location.href='/'">Home</button>
-    <div class="menuBox">
-        <button id="career-btn">My career</button>
-        <div id="career-options" class="dropdown-content">
-            <a href="index.php?page=cv">CV</a>
-            <a href="index.php?page=projects">Projects</a>
-        </div>
-    </div>
+    <button onclick="window.location.href='index.php?page=cv'">CV</button>
+    <button onclick="window.location.href='index.php?page=projects'">Projects</button>
     <button onclick="window.location.href='index.php?page=contact'">Contact</button>
     <div id="login">
         <button onclick="window.location.href='index.php?page=login'"><?php 
             if (isset($_SESSION['user'])) {
-                echo 'Profile';
+                if ($_SESSION['user']['first_name'] === null && $_SESSION['user']['last_name'] === null) {
+                    echo 'Profile';
+                } else {
+                  echo $_SESSION['user']['first_name'] . ' ' . $_SESSION['user']['last_name'];  
+                }
             } else {
                 echo 'Login';
             }
