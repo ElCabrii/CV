79f5a218003b644c55fc6bc2cc4d0e32373db3c2
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-10-31 18:59:59 +0100

    add form.js; implement functions to manage display of experience, education, skill, and project forms

---
+++ app/assets/js/form.js
@@ -0,0 +1,32 @@
+function openExperienceForm() {
+    document.getElementById("experience-form").style.display = "block";
+}
+
+function closeExperienceForm() {
+    document.getElementById("experience-form").style.display = "none";
+}
+
+function openEducationForm() {
+    document.getElementById("education-form").style.display = "block";
+}
+
+function closeEducationForm() {
+    document.getElementById("education-form").style.display = "none";
+}
+
+function openSkillForm() {
+    document.getElementById("skill-form").style.display = "block";
+}
+
+function closeSkillForm() {
+    document.getElementById("skill-form").style.display = "none";
+}
+
+function openProjectForm() {
+    document.getElementById("project-form").style.display = "block";
+}
+
+function closeProjectForm() {
+    document.getElementById("project-form").style.display = "none";
+}
+
