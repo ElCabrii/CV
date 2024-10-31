e7f8035ebacead33b54b242d6d5ffe65a7f9f1b1
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-10-31 18:59:56 +0100

    remove project creation functions; eliminate unused code from projects.js

--- app/assets/js/projects.js
+++ app/assets/js/projects.js
@@ -1,12 +1,3 @@
-
-function openProjectCreation() {
-    document.getElementById("project-creation").style.display = "block";
-}
-
-function closeProjectCreation() {
-    document.getElementById("project-creation").style.display = "none";
-}
-
 function joinProject(id){
     window.location.href ="/index.php?page=redirect&action=joinProject&id=" + id;
 } 
