26f1cdca8932a923eba93e1929ce8c30d657fe01
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-10-31 18:59:10 +0100

    add delete action handling; implement logic for deleting experiences, education, and skills in redirectController

--- app/controllers/redirectController.php
+++ app/controllers/redirectController.php
@@ -2,7 +2,7 @@
 
 
 
-if (!isset($_GET['action'])) {
+if (!isset($_GET['action']) || in_array($_GET['action'], ['logout', 'joinProject', 'leaveProject', 'delete'], true) === false) {
     header('Location: /index.php?page=404');
     exit;
 }
@@ -43,6 +43,42 @@ if ($_GET['action'] === 'leaveProject'){
         if (leaveProject($_GET['id'])) {
             header('Location: /index.php?page=projects');
             exit;
+        } else {
+            header('Location: /index.php?page=404');
+        }
+    }
+}
+
+if ($_GET['action'] === 'delete'){
+    if (!isset($_SESSION['user'])) {
+        header('Location: /index.php?page=login');
+        exit;
+    } else if (!isset($_GET['type']) || !isset($_GET['id'])) {
+        header('Location: /');
+        exit;
+    } else {
+        require './models/cvModel.php';
+        if ($_GET['type'] === 'experience') {
+            if (deleteExperience($_GET['id'])) {
+                header('Location: /index.php?page=cv');
+                exit;
+            } else {
+                header('Location: /index.php?page=404');
+            }
+        } else if ($_GET['type'] === 'education') {
+            if (deleteEducation($_GET['id'])) {
+                header('Location: /index.php?page=cv');
+                exit;
+            } else {
+                header('Location: /index.php?page=404');
+            }
+        } else if ($_GET['type'] === 'skill') {
+            if (deleteSkill($_GET['id'])) {
+                header('Location: /index.php?page=cv');
+                exit;
+            } else {
+                header('Location: /index.php?page=404');
+            }
         } else {
             header('Location: /index.php?page=404');
         }
