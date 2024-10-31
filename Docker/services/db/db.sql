f5322f3786530bbbba83dfdc5a188917a7a06243
ElCabrii <144121693+ElCabrii@users.noreply.github.com>
2024-10-31 18:57:32 +0100

    update database schema; add experience, education, and skill tables with foreign key constraints

--- Docker/services/db/db.sql
+++ Docker/services/db/db.sql
@@ -1,12 +1,15 @@
+-- Active: 1729860062337@@127.0.0.1@3306@cv_db
 CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role ENUM('admin', 'user') DEFAULT 'user',
+    role ENUM('admin', 'user') DEFAULT 'user' NOT NULL,
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO user (email, first_name, last_name, password, role) VALUES ('gab.debure@gmail.com', 'Gabriel', 'De Bure', 'root', 'admin');
 
 CREATE TABLE project (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -31,7 +34,34 @@ CREATE TABLE cv (
     user_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    skills JSON NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
+CREATE TABLE experience (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE education (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE skill (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    level ENUM('beginner', 'intermediate', 'advanced', 'expert') DEFAULT 'beginner' NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
